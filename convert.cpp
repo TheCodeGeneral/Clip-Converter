5dda860c08a354593791ad0c263c96d71283b476
TheCodeGeneral <camrpierce@gmail.com>
2023-03-31 02:05:05 -0500

    Update convert.cpp

--- convert.cpp
+++ convert.cpp
@@ -9,12 +9,12 @@
 
 namespace fs = std::filesystem;
 
-void ffmpeg(std::string &rp_config, std::string export_dir, std::string extension);
+void ffmpeg(std::string const& rpConfPath, std::string const& exportDir, std::string const& extension);
 void print_help();
 int main(int argc, char** argv)
 {
     std::string rpConfPath{};
-    std::string export_path{};
+    std::string exportDir{};
     std::string extension{};
     switch(argc)
     {
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
             rpConfPath = argv[1];
             if(strncmp("-o", argv[2],2) == 0 || strncmp("--output", argv[2], 8) == 0)
             {
-                export_path = argv[3];
+                exportDir = argv[3];
                 extension = "mp3";
             }
             else
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
             rpConfPath = argv[1];
             if(strncmp("-o", argv[2], 2) == 0 || strncmp("--output", argv[2], 8) == 0)
             {
-                export_path = argv[3];
+                exportDir = argv[3];
                 if(strncmp("-e", argv[4], 2) == 0 || strncmp("--extension", argv[4], 11) == 0)
                     extension = argv[5];
                 else
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
             {
                 extension = argv[3];
                 if(strncmp("-o", argv[4], 2) == 0 || strncmp("--output", argv[4], 8) == 0)
-                    export_path = argv[5];
+                    exportDir = argv[5];
                 else
                 {
                     std::cout << "Invalid Usage" << std::endl;
@@ -74,11 +74,20 @@ int main(int argc, char** argv)
             exit(-1);
     }
 
-    std::ifstream t(rpConfPath);
-    std::string rp_config((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
     // Verify valid extension
     // Verify valid directory
-    ffmpeg(rp_config, export_path, extension);
+    ffmpeg(rpConfPath, fs::absolute(exportDir).string(), extension);
+
+}
+
+void print_help()
+{
+    int descSpacing = 40;
+    std::cout << "Usage: clip_converter <rp_soundboard path> -o <output path> [OPTIONS]...\n\n";
+    std::cout << "Options:\n";
+    std::cout << std::left << std::setw(descSpacing) << "  -o, --output <directory>" << "Output directory\n";
+    std::cout << std::left << std::setw(descSpacing) << "  -e, --extention [extension]" << "Converted file format (default=mp3)\n";
+    std::cout << std::left << std::setw(descSpacing) << "  -h, --help" << "Print this help message" << std::endl;
 }
 /*
 Example 
@@ -92,20 +101,17 @@ Example
 1\cropStopAfterAt=0
 1\cropStopValue=0
 1\cropStopUnit=1
+...
 */
-
-void print_help()
+void ffmpeg(std::string const& rpConfPath, std::string const& exportDir, std::string const& extension)  
 {
-    int descSpacing = 40;
-    std::cout << "Usage: clip_converter <rp_soundboard path> -o <output path> [OPTIONS]...\n\n";
-    std::cout << "Options:\n";
-    std::cout << std::left << std::setw(descSpacing) << "  -o, --output <directory>" << "Output directory\n";
-    std::cout << std::left << std::setw(descSpacing) << "  -e, --extention [extension]" << "Converted file format (default=mp3)\n";
-    std::cout << std::left << std::setw(descSpacing) << "  -h, --help" << "Print this help message" << std::endl;
-}
+    // Read configuration into rpConfig
+    std::fstream confIn(rpConfPath);
+    std::string rpConfig((std::istreambuf_iterator<char>(confIn)), std::istreambuf_iterator<char>());
+    confIn.close();
 
-void ffmpeg(std::string &rp_config, std::string export_dir, std::string extension)  
-{
+    std::string newRpConfig = rpConfig;
+
     std::string clipPath{};
     std::string customText{};
     bool cropEnabled;
@@ -116,10 +122,12 @@ void ffmpeg(std::string &rp_config, std::string export_dir, std::string extensio
     std::string cropStop;
 
     std::regex re("(?:.+path=(.+)\\n.+customText=(.*)\\n.+\\n.+\\n.+cropEnabled=(.+)\\n.+cropStartValue=(\\d+)\\n.+cropStartUnit=(\\d)\\n.+cropStopAfterAt=(\\d)\\n.+cropStopValue=(\\d+)\\n.+cropStopUnit=(\\d))");
-    for(std::sregex_iterator i = std::sregex_iterator(rp_config.begin(), rp_config.end(), re); i != std::sregex_iterator(); ++i )
+    for(std::sregex_iterator i = std::sregex_iterator(rpConfig.begin(), rpConfig.end(), re); i != std::sregex_iterator(); ++i )
     {
         std::smatch m = *i;
         clipPath = m[1].str();
+        
+        // Check if clip to be converted exists
         if(!fs::exists(fs::path(clipPath)))
             continue;
         
@@ -134,6 +142,7 @@ void ffmpeg(std::string &rp_config, std::string export_dir, std::string extensio
         std::string timeConstrains{};
         std::string fArgs = "-vn ";
 
+        // Setup crop parameter for ffmpeg
         if(cropEnabled)
         {
             if(cropStartUnit == '1')
@@ -158,14 +167,61 @@ void ffmpeg(std::string &rp_config, std::string export_dir, std::string extensio
         if(customText.empty())
             customText = clipPath;
 
-        fs::path exp = (fs::path(export_dir) / fs::path(customText).filename()).replace_extension(fs::path(extension));
+        // Converted file path
+        fs::path exp = (fs::path(exportDir) / fs::path(customText).filename()).replace_extension(fs::path(extension));
+
+
+        // Make duplicate if converted clip already exists
         if(fs::exists(exp))
-            //exp = exp.root_path() / fs::path(exp.stem().string() + "(1)").replace_extension(exp.extension());
-            continue;
+        {
+            std::string filename = exp.stem().string();
+            std::smatch m;
+            int copyNum; 
+            std::string filenameBase;
+            // Check if copy already exists
+            if(std::regex_search(filename, m, std::regex(".+ Copy - \\((\\d+)\\)")))
+            {
+                // Increase copy number by 1 in ( )
+                copyNum = stoi(m[1].str()) + 1;
+                filenameBase = std::regex_replace(filename,std::regex("(.+ Copy - \\()(\\d+)\\)"), "$1");
+                filename = filenameBase + std::to_string(copyNum) + ")";
+            }
+            else
+                filename += " Copy - (1)";
+
+            exp = exp.parent_path() / fs::path(filename).replace_extension(exp.extension());
+
+            while(fs::exists(exp))
+            {
+                filename = filenameBase + std::to_string(++copyNum) + ")";
+                exp = exp.parent_path() / fs::path(filename).replace_extension(exp.extension());
+            }
+        }
+
+
+        // Escape the / delimter for regex in clip path
+        std::regex clipRegexFormat(std::regex_replace(clipPath, std::regex("\\/"), "\\/", std::regex_constants::match_any));
+
+        // Wrap path in quotes in case path contains spaces
         std::string exportPath = "\"" + exp.string() + "\"";
         clipPath = "\"" + clipPath + "\" ";
 
-        system(("ffmpeg.exe -i " + clipPath + timeConstrains + fArgs + exportPath).c_str());
+        system(("ffmpeg.exe -i " + clipPath + timeConstrains + fArgs + exportPath ).c_str());
+
+        // Repalce Windows path sperators
+        exportPath = std::regex_replace(exp.string(), std::regex("\\\\"), "/", std::regex_constants::match_any);
+        // Replace new clip path into config
+        newRpConfig = std::regex_replace (newRpConfig, clipRegexFormat, exportPath, std::regex_constants::match_any);
     }
 
+    // Disable crop on new rp config
+    newRpConfig = std::regex_replace (newRpConfig, std::regex("cropEnabled=true"), "cropEnabled=false", std::regex_constants::match_any);
+
+    // Write changes to rp config 
+    std::ofstream confOut(rpConfPath, std::ios::trunc);
+    confOut << newRpConfig;
+    confOut.close();
+
+    std::cout << newRpConfig << std::endl;
+    system("pause");
 }
